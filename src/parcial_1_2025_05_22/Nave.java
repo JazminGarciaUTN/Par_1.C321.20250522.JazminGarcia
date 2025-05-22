cf9c850cc898768cecfe5091a86c18c298f430d7
JazminGarciaUTN <jazmingciia@gmail.com>
2025-05-22 20:01:29 -0300

    primer desarrollo

---
+++ src/parcial_1_2025_05_22/Nave.java
@@ -0,0 +1,56 @@
+
+package parcial_1_2025_05_22;
+
+/**
+ *
+ * @author Jazmin
+ */
+public abstract class Nave {
+    protected String nombre;
+    protected int capacidadTripulacion;
+    protected int anioLanzamiento;
+
+    public Nave(String nombre, int capacidadTripulacion, int anioLanzamiento) {
+        this.nombre = nombre;
+        this.capacidadTripulacion = capacidadTripulacion;
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    public void iniciarExploracion(){
+        System.out.println("Iniciando exploración");
+    }
+    
+    public void mostrar(){
+        System.out.println("Nombre: " + nombre + " Capacidad de tripulación: " + capacidadTripulacion 
+                + " Año de lanzamiento: " + anioLanzamiento);
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidadTripulacion() {
+        return capacidadTripulacion;
+    }
+
+    public void setCapacidadTripulacion(int capacidadTripulacion) {
+        this.capacidadTripulacion = capacidadTripulacion;
+    }
+
+    public int getAnioLanzamiento() {
+        return anioLanzamiento;
+    }
+
+    public void setAnioLanzamiento(int anioLanzamiento) {
+        this.anioLanzamiento = anioLanzamiento;
+    }
+    
+    
+    
+    
+}
